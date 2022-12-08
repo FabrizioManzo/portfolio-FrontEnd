b8732fa9b77af28989a2713b3c7d930e9822e006
FabrizioManzo <113938801+FabrizioManzo@users.noreply.github.com>
2022-12-08 19:04:13 -0300

    Create presentacion.js

---
+++ presentacion.js
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title> MI PRIMER TITULO </title>
+<style>
+p {
+    font-family: Arial;
+    font-size: 50px;
+    font-weight: bold;
+    font-style: italic;
+    color: #0000ff;
+    background-color: #ffffff;
+    border-style: solid;
+    border-color: #f50c0c;
+    border-width: 2px;
+    margin: 14px;
+    padding: 20px;
+    border-radius: 14px;
+}
+
+f {
+      font-family: Franklin Gothic Medium;
+    font-size: 25px;
+    font-weight: bold;
+    font-style: italic;
+    color: #ffffff;
+    background-color: #00ff00;
+    border-style: solid;
+    border-color: #f50c0c;
+    border-width: 2px;
+    margin: 14px;
+    padding: 20px;
+    border-radius: 14px;
+  
+}
+
+</style>
+  </head>
+
+<body>
+
+<p> Presentación del restaurante de comida rápida! (Proyecto en el cual estaré trabajando) </p>
+<f> Interfaz gráfica: Tkinter 
+    Db: Sqlite3
+      </f>
+
+</body>
+
+</html>
